e8e728465508227235b232f9ea81066e242857e7
[Brandon] <desselle.brandon2@gmail.com>
2020-02-13 15:06:40 -0600

    Create Background.tsx

---
+++ basic_wallet_p/wallet/src/components/Background.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Background: React.FC = () => {
+  return (
+    <div className="float3 ">
+      <div className="circle circle1"></div>
+      <div className="circle circle2"></div>
+      <div className="circle circle3"></div>
+      <div className="circle circle4"></div>
+    </div>
+  );
+};
+
+export default Background;
